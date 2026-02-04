36413ee5b88d4bafc9751d0909fda46de27b3ebc
Kério Luqueni <kerio@kerioluqueni.com>
2026-02-04 23:11:44 +0100

    Configure serverless environment with Neon

--- lib/actions/product.actions.ts
+++ lib/actions/product.actions.ts
@@ -1,17 +1,7 @@
 "use server"
-import { PrismaClient } from "../generated/prisma/client"
-import { PrismaPg } from "@prisma/adapter-pg"
-import "dotenv/config"
+import { prisma } from "@/db/prisma"
 import { convertToPlainObject } from "../utils"
 import { LATEST_PRODUCTS_LIMIT } from "../constants"
-
-const adapter = new PrismaPg({
-  connectionString: process.env.DATABASE_URL!,
-})
-
-const prisma = new PrismaClient({
-  adapter,
-})
 
 // Get latest products
 export async function getLatestProducts() {
