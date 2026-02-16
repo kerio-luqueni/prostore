9c8023b8ceae250168594d7a0ec2984602e27ebf
Kério Luqueni <kerio@kerioluqueni.com>
2026-02-16 21:04:49 +0100

    Add user button and sign out

--- components/shared/header/menu.tsx
+++ components/shared/header/menu.tsx
@@ -1,6 +1,6 @@
 import Link from "next/link"
 import ModeToggle from "./mode-toggle"
-import { EllipsisVertical, ShoppingCart, UserIcon } from "lucide-react"
+import { EllipsisVertical, ShoppingCart } from "lucide-react"
 import { Button } from "@/components/ui/button"
 import {
   Sheet,
@@ -9,6 +9,7 @@ import {
   SheetTitle,
   SheetTrigger,
 } from "@/components/ui/sheet"
+import UserButton from "./user-button"
 
 const Menu = () => {
   return (
@@ -20,11 +21,7 @@ const Menu = () => {
             <ShoppingCart /> Cart
           </Link>
         </Button>
-        <Button asChild>
-          <Link href="/sign-in">
-            <UserIcon /> Sign In
-          </Link>
-        </Button>
+        <UserButton />
       </nav>
       <nav className="md:hidden">
         <Sheet>
@@ -40,11 +37,7 @@ const Menu = () => {
                 <ShoppingCart /> Cart
               </Link>
             </Button>
-            <Button asChild>
-              <Link href="/sign-in">
-                <UserIcon /> Sign In
-              </Link>
-            </Button>
+            <UserButton />
             <SheetDescription></SheetDescription>
           </SheetContent>
         </Sheet>
